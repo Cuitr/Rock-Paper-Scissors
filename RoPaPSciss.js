d4a888121942a7d338b28fcf88d8d0c1ecf8eb00
Vinh Tran <tranthanhvinhlxag1995@gmail.com>
2023-09-27 12:10:31 +0700

    modified playGround1 function

--- RoPaPSciss.js
+++ RoPaPSciss.js
@@ -13,6 +13,42 @@ function getComputerChoice(){
     }
 }
 let playerGuess = prompt(`Rock, Paper, Scissors. What's choice?`);
-function playGround1(playerSelection, ComputerSelection){
-    
+function playGround1(computerSelection, playerSelection){
+    while(playerSelection.toLowerCase() != `rock` && playerSelection.toLowerCase() != `paper` && playerSelection.toLowerCase() != `scissors`){
+        playerSelection = prompt(`Invalid data! Please try again!`);
+    }
+    switch(computerSelection == `rock`){
+        case (playerSelection.toLowerCase() === `rock`):
+            return `Computer picked Rock, we have a raw!`;
+            break;
+        case (playerSelection.toLowerCase() === `paper`):
+            return `Computer picked Rock, Paper beat Rock, you win!`;
+            break;
+        case (playerSelection.toLowerCase() === `scissors`):
+            return `Computer picked Rock, Rock beat scissors, computer win!`;
+            break;
+    }
+    switch(computerSelection == `paper`){
+        case (playerSelection.toLowerCase() === `rock`):
+            return `Computer picked Paper, Paper beat Rock, computer win!`;
+            break;
+        case (playerSelection.toLowerCase() === `paper`):
+            return `Computer picked Paper, we have a raw!`;
+            break;
+        case (playerSelection.toLowerCase() === `scissors`):
+            return `Computer picked Paper, Scissors beat Paper , you win!`;
+            break;
+    }
+    switch(computerSelection == `scissors`){
+        case (playerSelection.toLowerCase() === `rock`):
+            return `Computer picked Scissors, Rock beat scissors, you win!`;
+            break;
+        case (playerSelection.toLowerCase() === `paper`):
+            return `Computer picked Scissors, Scissors beat Paper, computer win!`;
+            break;
+        case (playerSelection.toLowerCase() === `scissors`):
+            return `Computer picked Scissors, we have a raw!`;
+            break;
+    }
 }
+playGround1(getComputerChoice(), playerGuess);
