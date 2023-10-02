15c25a560dca658bfa852ed38f07e3eecb37417d
Vinh Tran <tranthanhvinhlxag1995@gmail.com>
2023-10-03 00:30:55 +0700

    use if else instead of switch statement

--- RoPaPSciss.js
+++ RoPaPSciss.js
@@ -3,52 +3,46 @@ function getComputerChoice(){
     switch(randomChoice){
         case 0: 
             return `rock`;
-            break;
+            
         case 1:
             return `paper`;
-            break;
+            
         case 2:
             return `scissors`;
-            break;
+            
     }
 }
-let playerGuess = prompt(`Rock, Paper, Scissors. What's choice?`);
-function playGround1(computerSelection, playerSelection){
+
+function playGround1(computerSelection){
+    let playerSelection = prompt(`Rock, Paper, Scissors. What's choice?`);
     while(playerSelection.toLowerCase() != `rock` && playerSelection.toLowerCase() != `paper` && playerSelection.toLowerCase() != `scissors`){
         playerSelection = prompt(`Invalid data! Please try again!`);
     }
-    switch(computerSelection == `rock`){
-        case (playerSelection.toLowerCase() === `rock`):
+    if(computerSelection == `rock`){
+        if(playerSelection.toLowerCase() == `rock`){
             return `Computer picked Rock, we have a raw!`;
-            break;
-        case (playerSelection.toLowerCase() === `paper`):
-            return `Computer picked Rock, Paper beat Rock, you win!`;
-            break;
-        case (playerSelection.toLowerCase() === `scissors`):
-            return `Computer picked Rock, Rock beat scissors, computer win!`;
-            break;
-    }
-    switch(computerSelection == `paper`){
-        case (playerSelection.toLowerCase() === `rock`):
-            return `Computer picked Paper, Paper beat Rock, computer win!`;
-            break;
-        case (playerSelection.toLowerCase() === `paper`):
+        }else if (playerSelection.toLowerCase() == `paper`){
+            return `Computer picked Rock, Paper beat Rock, You win!`;
+        }else if(playerSelection.toLowerCase() == `scissors`){
+            return `Computer picked Rock, Rock beat scissors, Computer win!`;
+        }
+    }else if(computerSelection == `paper`){ 
+        if(playerSelection.toLowerCase() == `rock`){
+            return `Computer picked Paper, Paper beat Rock, Computer win!`;
+        }else if(playerSelection.toLowerCase() == `paper`){
             return `Computer picked Paper, we have a raw!`;
-            break;
-        case (playerSelection.toLowerCase() === `scissors`):
-            return `Computer picked Paper, Scissors beat Paper , you win!`;
-            break;
-    }
-    switch(computerSelection == `scissors`){
-        case (playerSelection.toLowerCase() === `rock`):
-            return `Computer picked Scissors, Rock beat scissors, you win!`;
-            break;
-        case (playerSelection.toLowerCase() === `paper`):
-            return `Computer picked Scissors, Scissors beat Paper, computer win!`;
-            break;
-        case (playerSelection.toLowerCase() === `scissors`):
-            return `Computer picked Scissors, we have a raw!`;
-            break;
+        }else if(playerSelection.toLowerCase() == `scissors`){
+            return `Computer picked Paper, Scissors beat Paper , You win!`;
+        }
+    }else{
+        if(playerSelection.toLowerCase() == `rock`){
+            return `Computer picked Scissors, Rock beat scissors, You win!`;
+        }else if(playerSelection.toLowerCase() == `paper`){
+            return `Computer picked Scissors, Scissors beat Paper, Computer win!`;
+        }else if(playerSelection.toLowerCase() == `scissors`){
+            return `Computer picked Scissors, we have a raw!`;      
     }
 }
-playGround1(getComputerChoice(), playerGuess);
+}
+
+playGround1(getComputerChoice());
